b872091722da7a8b1f05ce672f2b6c05a9c3f5a4
Ricardo Caldeira <user_raro63@tribanco.com.br>
2026-04-13 17:50:23 -0300

    Add password change functionality with validation and user interface

--- src/main/java/med/voll/web_application/domain/usuario/UsuarioService.java
+++ src/main/java/med/voll/web_application/domain/usuario/UsuarioService.java
@@ -7,6 +7,8 @@ import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import java.util.UUID;
+
 @Service
 public class UsuarioService implements UserDetailsService {
 
@@ -24,9 +26,11 @@ public class UsuarioService implements UserDetailsService {
                 .orElseThrow(() -> new UsernameNotFoundException("O usuário não foi encontrado!"));
     }
 
-    public Long salvarUsuario(String nome, String email, String senha, Perfil perfil) {
-        String senhaCriptografada = passwordEncoder.encode(senha);
-        Usuario usuario = usuarioRepository.save(new Usuario(nome, email, senhaCriptografada, perfil));
+    public Long salvarUsuario(String nome, String email, Perfil perfil) {
+        String primeiraSenha = UUID.randomUUID().toString().substring(0, 8);
+        System.out.println("Senha gerada: " + primeiraSenha);
+        String senhaCriptografada = passwordEncoder.encode(primeiraSenha);
+        var usuario =usuarioRepository.save(new Usuario(nome, email, senhaCriptografada, perfil));
         return usuario.getId();
     }
 
